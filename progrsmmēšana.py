4994e57198b266c2b6db05766af572f366f85808
zimelemonta <ziemelemonta@gmail.com>
2024-10-11 12:16:12 +0300

    Create progrsmmēšana.py

---
+++ progrsmmēšana.py
@@ -0,0 +1,17 @@
+try:
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=int(input("Enter"b:))
+except VlueError:
+print('a or b or c=int')
+
+D=b**2-4*a*c
+x1=(-b+D**0,5)/(2*a)
+x2-(-b-D**05)/(2*a)
+print(x1)
+print(x2)
+
+
+
+
+
